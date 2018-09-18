be853db112fe1eaf93b234865eedafbe491f5d1d
Ryan Kwong <ryankwong478@gmail.com>
2018-09-17 17:37:20 -0700

    Ryan's TeleOp

---
+++ App/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ryan_TeleOp.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp (name = "Ryan TeleOp", group = "TeleOp")
+@Disabled
+
+public class Ryan_TeleOp extends LinearOpMode {
+
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode(){
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        //this is hard code, switch to other method that is not hard code
+        while(opModeIsActive()){
+            if(gamepad1.left_stick_y < 0){
+                leftMotor.setPower(-1);
+                //rightMotor.setPower(-1);
+            }else if (gamepad1.left_stick_y > 0){
+                leftMotor.setPower(1);
+                //rightMotor.setPower(1);
+            }else{
+                leftMotor.setPower(0); //idle at 0
+            }
+            if(gamepad1.right_stick_y < 0){
+                //leftMotor.setPower(-1);
+                rightMotor.setPower(-1);
+            }else if (gamepad1.right_stick_y > 0){
+                //leftMotor.setPower(1);
+                rightMotor.setPower(1);
+            }else{
+                rightMotor.setPower(0); //idle at 0
+            }
+        }
+        //what's next
+    }
+}
