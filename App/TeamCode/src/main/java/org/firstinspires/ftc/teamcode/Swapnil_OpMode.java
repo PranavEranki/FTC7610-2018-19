160409515674d005a198e9e45a88f1edce7b2d4d
swapnilddas <swapnilddas@outlook.com>
2018-09-17 17:36:07 -0700

    testing swap

---
+++ App/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swapnil_OpMode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name="Final Tele-Op", group="Linear Opmode")
+public class Swapnil_OpMode extends TeleOp {
+
+    private DcMotor leftmotor;
+
+    public DcMotor getLeftmotor() {
+        return leftmotor;
+    }
+
+    rightMotor = hardwareMap.get.dcmotor("right_motor");
+         = hardwareMap.get.dcmotor("left_motor");
+
+public void runOpmode
+        telemetry.data()
