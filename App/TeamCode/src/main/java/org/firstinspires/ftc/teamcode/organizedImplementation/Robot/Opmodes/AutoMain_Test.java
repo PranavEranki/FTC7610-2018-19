e158d6f1e4f8f2f5c63e9063814c51906bf55bf1
PranavEranki <pranav.eranki@gmail.com>
2018-10-15 14:26:33 -0700

    pls

--- App/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/organizedImplementation/Robot/Opmodes/AutoMain_Test.java
+++ App/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/organizedImplementation/Robot/Opmodes/AutoMain_Test.java
@@ -29,6 +29,14 @@ public class AutoMain_Test extends LinearOpMode {
     BNO055IMU imu;
     public Orientation angles;
 
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
     @Override
     public void runOpMode() {
         gameState = 0;
@@ -77,8 +85,8 @@ public class AutoMain_Test extends LinearOpMode {
             //driveToDepot();
             motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            //
-        // break;
+
+        //break;
 
         //}
     }
