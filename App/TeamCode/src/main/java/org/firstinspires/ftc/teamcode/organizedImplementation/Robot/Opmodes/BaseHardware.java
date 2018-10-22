44f95aa36279a5f96d64c26ab646b27542faaa54
PranavEranki <pranav.eranki@gmail.com>
2018-10-21 19:18:19 -0700

    Starting gyro test code

---
+++ App/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/organizedImplementation/Robot/Opmodes/BaseHardware.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.organizedImplementation.Robot.Opmodes;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class BaseHardware {
+
+    public void init(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.initialize(hardwareMap, telemetry);
+    }
+    private void initialize(HardwareMap hardwareMap, Telemetry telemetry){
+
+    }
+
+
+
+}
