1d34e3c67148011dcad4f5dc3cc3a2f03fa376a9
swang1111 <shannonwang1029@gmail.com>
2018-10-31 19:40:11 -0700

    yeeeet

---
+++ App/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/organizedImplementation/Robot/Opmodes/AutonomousTurn.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.organizedImplementation.Robot.Opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+
+@Autonomous(name= "Autonomous Turn", group="Autonomous")
+public class AutonomousTurn extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+    @Override
+    public void runOpMode() {
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        leftMotor = hardwareMap.dcMotor.get("left_motor");
+        rightMotor = hardwareMap.dcMotor.get("right_motor");
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Turn right for 2 seconds
+        runtime.reset();
+
+        leftMotor.setPower(0.5);
+        rightMotor.setPower(-0.5);
+        while (opModeIsActive() && (runtime.seconds() < 2.0)) {
+            telemetry.addData("Path 2 - Turn Right", runtime.seconds());
+            telemetry.update();
+        }
+
+
+        //Stop
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);
+    }
+}
